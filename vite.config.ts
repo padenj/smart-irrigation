4d2bd9a863465dcedb7d3bc2684ef16b5bce8041
Jeremy Paden <jeremy@padenco.com>
2025-04-26 17:58:39 +0000

    update CSP

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import csp from 'vite-plugin-csp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    csp({
+      policy: {
+        'default-src': ["self"],
+        'script-src': ["self", "unsafe-inline", "https://static.cloudflareinsights.com", "https://cdn.weather.com"],
+        // Add other directives as needed
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: [
       'pigpio', 
